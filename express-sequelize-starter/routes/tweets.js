db4b614aaf4f61633ec0283b1763051b441a637e
marvin <marvinma.mw@gmail.com>
2021-05-26 16:45:41 -0400

    phase3Part3

--- express-sequelize-starter/routes/tweets.js
+++ express-sequelize-starter/routes/tweets.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
+const { check, validationResult } = require('express-validator');
 const db = require("../db/models");
 const { Tweet } = db;
 const router = express.Router();
 
-
+const tweetNotFoundError = (tweetId)=>{
+    const error = Error(`Tweet At ID ${tweetId} Not Found`);
+    error.title = "Tweet Not Found";
+    error.status = 404;
+    return error
+}
 
 router.get("/", asyncHandler(async (req, res) => {
     const tweets = await Tweet.findAll();
@@ -17,13 +23,23 @@ router.get("/:id(\\d+)", asyncHandler(async (req, res, next) => {
     const tweet = await Tweet.findByPk(parseInt(tweetId, 10));
 
     if (tweet) {
-        await res.json({ tweet });
+         res.json({ tweet });
     } else {
-        //TODO
+         next(tweetNotFoundError(tweetId))
     }
 }));
 
+const userValidators = [
+    check("message")
+        .exists({checkFalsy:true})
+        .withMessage("PLEASE PROVIDE A MESSAGE")
+        .isLength({ max:280 })
+];
 
+
+router.post("/",asyncHandler(async(req,res)=>{
+    const validatorError = validationResult(req)
+}))
 
 
 
