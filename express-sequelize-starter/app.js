3ed1b9f97e3d509827505807257308e5ec85c275
IrinaAmzashvili <irina.amzashvili@gmail.com>
2021-05-26 12:16:13 -0600

    setting up test routes

--- express-sequelize-starter/app.js
+++ express-sequelize-starter/app.js
@@ -2,8 +2,13 @@ const express = require("express");
 const morgan = require("morgan");
 const { environment } = require('./config');
 const app = express();
+const indexRouter = require('./routes/index');
+const tweetsRouter = require('./routes/tweets');
 
 app.use(morgan("dev"));
+app.use(express.json());
+app.use('/', indexRouter);
+app.use('/tweets', tweetsRouter);
 
 app.get("/", (req, res) => {
   res.send("Welcome to the express-sequelize-starter!");
