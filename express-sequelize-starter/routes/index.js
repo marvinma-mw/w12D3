caa67097d791de0143406b179742b0bc1789bf8a
marvin <marvinma.mw@gmail.com>
2021-05-26 15:56:41 -0400

    phase3Part1

--- express-sequelize-starter/routes/index.js
+++ express-sequelize-starter/routes/index.js
@@ -2,4 +2,25 @@ const express = require('express');
 
 const router = express.Router();
 
+router.get("/", (req, res) => {
+    res.json({ message: "test root index" });
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 module.exports = router;
--- express-sequelize-starter/routes/tweets.js
+++ express-sequelize-starter/routes/tweets.js
@@ -1,5 +1,29 @@
 const express = require('express');
-
+const asyncHandler = require('express-async-handler');
+const db = require("../db/models");
+const { Tweet } = db;
 const router = express.Router();
+
+
+
+router.get("/", asyncHandler(async (req, res) => {
+    const tweets = await Tweet.findAll();
+    res.json({ tweets });
+  }));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports = router;
