6c93f5275b1357cc7584f3033d7fec25ee5c40f1
marvin <marvinma.mw@gmail.com>
2021-05-27 16:29:59 -0400

    phase one created routerPost

--- express-sequelize-starter/routes/tweets.js
+++ express-sequelize-starter/routes/tweets.js
@@ -13,20 +13,6 @@ const tweetNotFoundError = (tweetId)=>{
     return error
 }
 
-// const handleValidationErrors = (req, res, next) => {
-//     const validationErrors = validationResult(req);
-
-//     if (!validationErrors.isEmpty()) {
-//         const errors = validationErrors.array().map(error => error.msg);
-//         const err = Error('Bad request.');
-//         err.errors = errors;
-//         err.status = 404;
-//         err.title = 'Bad request.';
-//         return next(err);
-//     }
-//     next();
-// }
-
 router.get("/", asyncHandler(async (req, res) => {
     const tweets = await Tweet.findAll();
     res.json({ tweets });
--- express-sequelize-starter/routes/users.js
+++ express-sequelize-starter/routes/users.js
@@ -1,8 +1,50 @@
 const express = require('express');
 const asyncHanlder = require('express-async-handler');
+const bcrypt = require('bcryptjs')
 const { handleValidationErrors } = require('../utils.js');
-
+const { check } = require('express-validator');
+const db = require("../db/models");
 const router = express.Router();
+const { User } = db;
+
+
+const validateUsername = [
+    check('username')
+        .exists({ checkFalsy: true})
+        .withMessage("Please Provide A Username")
+];
+
+const validateEmailAndPassword = [
+    check("email")
+      .exists({ checkFalsy: true })
+      .isEmail()
+      .withMessage("Please provide a valid email."),
+    check("password")
+      .exists({ checkFalsy: true })
+      .withMessage("Please provide a password."),
+  ];
+
+
+router.post(
+    '/',
+    validateUsername,
+    validateEmailAndPassword,
+    handleValidationErrors,
+    asyncHanlder(async(req,res)=>{
+        const { username, email, password } = req.body;
+        const hashedPassword = await bcrypt.hash(password,10);
+        const user = await User.create({
+            username,
+            email,
+            hashedPassword
+        })
+
+}))
+
+
+
+
+
 
 
 module.exports = router;
